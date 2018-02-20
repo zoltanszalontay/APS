2d285f73e8bbb84dbb4c95f0ca087798e01b77f0
Zoltán Szalontay <zoltansz@hotmail.com>
2018-02-20 15:30:45 +0100

    Parsing header

--- AzurePriceScraper.py
+++ AzurePriceScraper.py
@@ -1,14 +1,14 @@
+# v2
 # -*- coding: utf-8 -*-
 import logging
-import bs4
-from bs4 import BeautifulSoup
 import urllib.request
+from bs4 import BeautifulSoup
 
 class AzurePriceList:
     """Scrape the actual IaaS prices from the Azure Price list webpage"""
 
     BaseURL = "https://azure.microsoft.com/en-us/pricing/details/virtual-machines/"
-    
+
     product_url = {
         "CentOS or Ubuntu Linux":                           "linux",
         "Red Hat Enterprise Linux":                          "red-hat",
@@ -33,12 +33,15 @@ class AzurePriceList:
         "SQL Server Web":                                    "sql-server-web"
     }
 
+    price = []
+
     def __init__(self, product_name, log_lvl=logging.INFO):
+        # Start logging
         self.log = logging.getLogger(__name__)
         if log_lvl is None:
             log_lvl = logging.WARNING
         self.log.setLevel(log_lvl)
-        if not len(self.log.handlers):
+        if len(self.log.handlers) == 0:
             self.log.addHandler(logging.StreamHandler())
         self.log.info("Product name: %s", product_name)
 
@@ -61,48 +64,28 @@ class AzurePriceList:
         #         print(cell)
         source = urllib.request.urlopen("https://azure.microsoft.com/en-us/pricing/details/virtual-machines/windows/").read()
         soup = BeautifulSoup(source, 'lxml')
-        rows = soup.find(class_="sd-table").find_all('tr')[1:]
-
-        for row in rows:
-            # cell = [i.text for i in row.find_all('td')[1:]]
-            cell = [i.text for i in row.find_all('td')[1:]]
-            print("cell:", cell)
-
-        # Process rows; 1st row is the table header
-        # i_row = 0
-        # for row in price_table.find("thead"):
-        #     self.log.info("Row #%d, length: %d", i_row, len(row))
-        #     self.log.info("Row: %s", row)
-        #     i_cell = 0
-        #     for cell in row:
-        #         if(type(cell) is not bs4.element.NavigableString:
-        #             # self.log.info("Cell type: %s", type(cell))
-        #             self.log.info("Cell[%d]: %s (type: %s)", i_cell, cell, type(cell))
-        #         i_cell = i_cell + 1
-        #     i_row = i_row + 1
-            # for row in price_table.findAll("tbody"):
         
+        row = soup.find(class_="sd-table").find_all('th')[1:]
+        i = 0
+        for cell in row:
+            self.log.info("cell[%d] = %s", i, cell.text)
+            self.price.append(cell.text)
+            i = i + 1
+        
+ #       for j in len(self.price):
+ #           self.log.info("price[%d] = %s", j, self.price[j])
 
-            #     cells = row.findAll("td")
-            #     if len(cells) == 5:
-            #         city = cells[0].find(text=True)            
-            #     if city == "Szentendre":
-            #         print("The level of " + river + " at " + city + " is " + level + " centimeters.")
-
-
-# machine_types = []
-# machine_properties = []
+#
+# Main
+# 
 
 # Start scraping, get prices
 apl = AzurePriceList("Windows Operating System", log_lvl=logging.INFO)
 apl.scrape()
 
-
-
-
-
-
-#  
+#
+#=====================================
+#
 # class="sd-table"
 #     <thead>
 #         <tr>
